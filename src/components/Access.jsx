cd6c1a0f465803ced45bfbabc47722d5a7941075
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-07-03 18:11:09 +0900

    ACCESS分を作成(PR指摘対応)

--- src/components/Access.jsx
+++ src/components/Access.jsx
@@ -19,8 +19,8 @@ const Access = () => {
                 <iframe
                     className='access__map'
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1620.6556502687954!2d139.7017094344435!3d35.66933613063013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c9fa9e2e881%3A0x35396adb5ba7ce03!2z44K444O844K644Ki44Kr44OH44Of44O85p2x5Lqs!5e0!3m2!1sja!2sjp!4v1728546610827!5m2!1sja!2sjp"
-                    frameborder="0"
-                    allowFullScreen=""
+                    frameBorder={0}
+                    allowFullScreen
                     loading='lazy'
                     referrerPolicy='no-referrer-when-downgrade'
                     title='チーズアカデミーTOKYOのMAP'
@@ -30,8 +30,8 @@ const Access = () => {
             <div className='access__info'>
                 <table className='access__table'>
                     <tbody>
-                        {companyInfo.map((info, index) => (
-                            <tr key={index}>
+                        {companyInfo.map((info) => (
+                            <tr key={info.label}>
                                 <th>{info.label}</th>
                                 <td>{info.value}</td>
                             </tr>
