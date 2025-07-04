f7d1bb7ef22e1c678b30f12c9299649c3d070b45
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-07-04 11:47:21 +0900

    Enable base path for Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/cheeseAcademy_react_sass/',
+  base: '/cheeseAcademy_react_sass/',
   plugins: [react()],
 })
